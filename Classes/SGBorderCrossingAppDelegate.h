2bb86c7e7e5c13a663310177c975198cc405a2ac
dsmith <derek@simplegeo.com>
2010-08-19 18:51:04 -0700

    Initial commit

---
+++ Classes/SGBorderCrossingAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  SGBorderCrossingAppDelegate.h
+//  SGBorderCrossing
+//
+//  Created by Derek Smith on 8/19/10.
+//  Copyright CrashCorp 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SGBorderCrossingAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
