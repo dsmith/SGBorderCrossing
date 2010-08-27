327190ae08526908b538a6971f49d3c0cd3fda7c
dsmith <derek@simplegeo.com>
2010-08-27 14:35:09 -0700

    Added license

--- Classes/SGBorderCrossingAppDelegate.m
+++ Classes/SGBorderCrossingAppDelegate.m
@@ -2,21 +2,48 @@
 //  SGBorderCrossingAppDelegate.m
 //  SGBorderCrossing
 //
-//  Created by Derek Smith on 8/19/10.
-//  Copyright CrashCorp 2010. All rights reserved.
+//  Copyright (c) 2009-2010, SimpleGeo
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without 
+//  modification, are permitted provided that the following conditions are met:
+//
+//  Redistributions of source code must retain the above copyright notice, 
+//  this list of conditions and the following disclaimer. Redistributions 
+//  in binary form must reproduce the above copyright notice, this list of
+//  conditions and the following disclaimer in the documentation and/or 
+//  other materials provided with the distribution.
+//  
+//  Neither the name of the SimpleGeo nor the names of its contributors may
+//  be used to endorse or promote products derived from this software 
+//  without specific prior written permission.
+//   
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+//  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+//  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS 
+//  BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+//  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE 
+//  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+//  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+//  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+//  Created by Derek Smith.
 //
 
 #import "SGBorderCrossingAppDelegate.h"
 
 @implementation SGBorderCrossingAppDelegate
-
 @synthesize window;
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////
 #pragma mark -
-#pragma mark Application lifecycle
+#pragma mark Application lifecycle 
+//////////////////////////////////////////////////////////////////////////////////////////////// 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+
+- (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
     
     // Override point for customization after application launch.
 	
@@ -26,55 +53,29 @@
 }
 
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    /*
-     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-     */
+- (void) applicationWillResignActive:(UIApplication*)application 
+{
 }
 
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    /*
-     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
-     */
+- (void) applicationDidEnterBackground:(UIApplication *)application 
+{
 }
 
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    /*
-     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
-     */
+- (void) applicationWillEnterForeground:(UIApplication*)application 
+{
 }
 
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    /*
-     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-     */
+- (void)applicationDidBecomeActive:(UIApplication*)application
+{
 }
 
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    /*
-     Called when the application is about to terminate.
-     See also applicationDidEnterBackground:.
-     */
+- (void) applicationWillTerminate:(UIApplication*)application 
+{
+    
 }
 
-
-#pragma mark -
-#pragma mark Memory management
-
-- (void)applicationDidReceiveMemoryWarning:(UIApplication *)application {
-    /*
-     Free up as much memory as possible by purging cached data objects that can be recreated (or reloaded from disk) later.
-     */
-}
-
-
-- (void)dealloc {
+- (void) dealloc
+{
     [window release];
     [super dealloc];
 }
